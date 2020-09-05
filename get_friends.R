e2b0a4aaf2b83868c7c3c886f1fba76548e48f11
Carson Zhang <carsonzhang4@gmail.com>
2020-09-04 21:20:04 -0500

    refactored core functionality into functions. driver program with usage example

--- get_friends.R
+++ get_friends.R
@@ -2,8 +2,14 @@ library(rtweet)
 library(readr)
 
 # whichever user you want to analyze
-username = "zhang_carson"
-
-friends = (get_friends(username)$user_id %>% lookup_users())$screen_name
-
-write_lines(friends, "friends_all.txt")
+gen_friends = function(username) {
+  friends = (get_friends(username)$user_id %>% lookup_users())$screen_name
+  
+  friends_file_name = paste("friends_all_", username, ".txt", sep = "")
+  
+  write_lines(friends, friends_file_name, append = FALSE)
+  
+  handled_users_file_name = paste("handled_users_", username, ".txt", sep = "")
+  
+  file.create(handled_users_file_name)
+}
